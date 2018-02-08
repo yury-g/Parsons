b00aaef3584c825d5527302aa541b459072bf5b8
YuryG <yury@pulsesensor.com>
2018-02-08 13:29:36 -0500

    Update Class3_Book_Notes.js

--- PSAM_1028/randomStuff/Class3_Book_Notes.js
+++ PSAM_1028/randomStuff/Class3_Book_Notes.js
@@ -2,15 +2,13 @@
 // Chapter 3
 var myFruitArray = ["Apple","Banana","Coconut","Divine Fruit", "Earlobe Grapes", "Figs"]
 
-undefined
 myFruitArray
-(6) ["Apple", "Banana", "Coconut", "Divine Fruit", "Earlobe Grapes", "Figs"]0: "Apple"1: "Banana"2: "Coconut"3: "Divine Fruit"4: "Earlobe Grapes"5: "Figs"length: 6__proto__: Array(0)
+
 myFruitArray[0] = "Pinapple"
 
-"Pinapple"
 myFruitArray
-(6) ["Pinapple", "Banana", "Coconut", "Divine Fruit", "Earlobe Grapes", "Figs"]
+
 myFruitArray[10] = "Zookeyknee"
 "Zookeyknee"
+
 myFruitArray
-(11) ["Pinapple", "Banana", "Coconut", "Divine Fruit", "Earlobe Grapes", "Figs", empty × 4, "Zookeyknee"]0: "Pinapple"1: "Banana"2: "Coconut"3: "Divine Fruit"4: "Earlobe Grapes"5: "Figs"10: "Zookeyknee"length: 11__proto__: Array(0)
