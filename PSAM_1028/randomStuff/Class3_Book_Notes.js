efbc6a0fbbb22ee4966b0fdd158e00c0bdb14d33
YuryG <yury@pulsesensor.com>
2018-02-08 13:44:01 -0500

    Update Class3_Book_Notes.js

--- PSAM_1028/randomStuff/Class3_Book_Notes.js
+++ PSAM_1028/randomStuff/Class3_Book_Notes.js
@@ -1,9 +1,16 @@
 
 // Chapter 3
+// 
 var myFruitArray = ["Apple","Banana","Coconut","Divine Fruit", "Earlobe Grapes", "Figs"]
 
+//call it
 myFruitArray
 
+// call an "element" by it's next 
+myFruitArray[2]
+
+
+//  Write over
 myFruitArray[0] = "Pinapple"
 
 myFruitArray
@@ -14,3 +21,21 @@ myFruitArray[10] = "Zookeyknee"
 
 // See What happens
 myFruitArray
+
+myFruitArray[20]
+undefined
+myFruitArray
+
+var myFruitArray = ["Apple","Banana","Coconut","Divine Fruit", "Earlobe Grapes", "Figs"]
+
+undefined
+myFruitArray[2]
+"Coconut"
+
+// Call Last Element In Array
+var lastElementInArray =  myFruitArray[myFruitArray.length-1]
+undefined
+lastElementInArray
+"Figs"
+
+// 
