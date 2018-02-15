649f87f4aa0f9d068addfeda083ba43167908440
YuryG <yury@pulsesensor.com>
2018-02-15 14:07:21 -0500

    Update Week4.js

--- PSAM_1028/ClassNotes/Week4.js
+++ PSAM_1028/ClassNotes/Week4.js
@@ -1,1 +1,32 @@
 
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Here Is the Page Title!!! 🤓 </title>
+</head>
+<body>
+
+
+<p>The is just "normal" text.  - 😎 - </p>
+
+<script type="text/javascript">
+	
+var myPhrase = "Hello Earthlings"
+console.log(myPhrase)
+
+
+var name = prompt("What is your name?")
+console.log("hello" + name)
+
+</script>
+
+<h1>This is the Biggest Heading  - 🤨 -  </h1>
+<h2>This is the 2nd Biggest Heading  - 🧐 -  </h2>
+
+
+</body>
+</html>
+
+
+
+----------------------------
