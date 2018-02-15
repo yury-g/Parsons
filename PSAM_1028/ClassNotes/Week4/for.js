9253b23217ce9ac9b83a84f1ddbdbfe8b37b9dd5
YuryG <yury@pulsesensor.com>
2018-02-15 17:51:53 -0500

    Add files via upload

---
+++ PSAM_1028/ClassNotes/Week4/for.js
@@ -0,0 +1,12 @@
+
+
+// for (setup; condition; increment){
+// 	do something 
+// } 
+
+var timesToSayHello
+for (var i = 0;  i < timesToSayHello; i++) {
+	console.log("Hello !")
+	timesToSayHello++
+}
+
---
+++ PSAM_1028/ClassNotes/Week4/while.js
@@ -0,0 +1,6 @@
+
+var sheepCounted = 0
+while (sheepCounted < 10 ){
+	console.log("I have counted " + sheepCounted + "sheep!")
+	sheepCounted++
+}
