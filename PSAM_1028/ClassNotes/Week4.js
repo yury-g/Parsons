272d9129a6221871b362e4ffce58189b669f5c9a
YuryG <yury@pulsesensor.com>
2018-02-15 14:20:35 -0500

    Update Week4.js

--- PSAM_1028/ClassNotes/Week4.js
+++ PSAM_1028/ClassNotes/Week4.js
@@ -25,3 +25,38 @@ console.log("hello  " + name + " welcome.")
 </body>
 </html>
 
+
+
+
+
+
+---_-----------
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Here Is the Page Title!!! ABC </title>
+</head>
+<body>
+
+
+<p>I'm BEFORE the script </p>
+
+<script type="text/javascript">
+	
+var myPhrase = "Hello Earthlings"
+console.log(myPhrase)
+
+
+var name = prompt("What is your name?")
+console.log("hello  " + name + " welcome.")
+
+</script>
+
+<p>I'm AFTER the script </p>
+
+
+</body>
+</html>
+
+
